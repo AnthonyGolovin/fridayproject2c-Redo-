0fd59824468f5dad3087a39d1c443861969b5618
AnthonyGolovin <AGOLOVAIN97@live.com>
2019-07-26 11:38:49 -0700

    Created a new document due to various errors that broke my previous project

---
+++ README.md
@@ -0,0 +1,13 @@
+Specs:
+
+The first thing we will create is a prompt prompting the user to enter a sentence. The user will enter a sentence in a "Console.ReadLine()" line and shortly after the user will again be prompted to type in a word they would like to find duplicates of in their sentence.
+
+The we will create is a space where we can store the user input i.e. Array, List, or Dictionary. (In  this case will use the array method).
+
+After creating the array we will use a for loop to go through the entire user input and look for duplicate words.  
+
+From there once we have collected the repeated word data, we will go through user input and return an integer of repeated words.
+
+After we have collected the integer of the repeated word, will will return that integer to the user.
+
+TEST: We will create a test that will test whether the function actually catches duplicate words without counting words within words.
---
+++ WordCounter.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+// using RepeatedWordCounter;
+
+namespace RepeatedWordCounterUI
+{
+  public class RepeatCounterUI
+  {
+      public static void Main()
+      {
+    //     //  List<UserInput> UserInputList = new List<UserInput>();
+    //     //  AddUserInput(UserInputList);
+         
+    //      Console.WriteLine("Enter A sentence");
+    //      string UserInputSentence = Console.ReadLine();
+        
+
+
+    //      Console.WriteLine("Enter a word");
+    //      string UserInputWord = Console.ReadLine();
+
+    //     // UserInput newUserInput = new UserInput();
+    //     // UserInputList.add(newUserInput);
+    //   }
+
+
+    // //   static void AddUserInput(List<UserInput>) UserInputList)
+    // //   {
+    // //       Console.WriteLine("Enter A sentence");
+    // //       Console.ReadLine("");
+
+    // //       Console.WriteLine("")
+    // //   }
+  }
+ }
+}
