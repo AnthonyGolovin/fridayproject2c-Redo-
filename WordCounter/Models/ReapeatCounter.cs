27431793cbcc45460942286b9ddcbdd4e380e367
AnthonyGolovin <AGOLOVAIN97@live.com>
2019-07-26 12:42:15 -0700

    Add for loop and user input array to track all inputs

---
+++ WordCounter/Models/ReapeatCounter.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+// using RepeatedWordCounter;
+
+namespace RepeatedWordCounter
+{
+  public class RepeatCounter
+  {
+    // string UserInputSentence;
+    // string UserInputWord;
+    public UserSentence(string UserInputSentence, string UserInputWord)//This function will be finding the duplicates.
+    {
+    string[] inputSplit = UserInputSentence.Split(" ");
+
+    for (string i = 0; i < inputSplit.Length; i++)
+    {
+      
+    }
+
+
+    }
+     
+  }
+}
